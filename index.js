f8ba5d54ad3543c0c9d9e35f06378f84ce8fc569
John Delaney <jdelaney@netbuilder.com>
2019-05-20 17:02:49 +0100

    add cors

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({
 let gcpUtils = require("./controllers/gcpController");
 console.log(gcpUtils)
 // TODO resolve gcp before continuing (async)
+console.log("process.env1", process.env)
 if(!process.env.PORT){
     async function gcpData(){
         var myData = await gcpUtils.gcp();
